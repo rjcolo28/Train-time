143c245c69b8a6ae710cfd9545dc4c1ed42186b6
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2018-12-19 11:54:20 -0500

    Added styling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,19 +17,55 @@ var trainName = "";
 var destination = "";
 var firstTime = "";
 var frequency = "";
+var nextTrain = "";
+var minutes = "";
+var currentTime = moment();
+
+function dataPull() {
+  trainbase.ref().on("child_added", function(snapshot){
+    var sv = snapshot.val();
+  
+    console.log(sv.name);
+    console.log(sv.destination);
+    console.log(sv.frequency);
+    console.log(sv.nextTrain);
+    console.log(sv.minutesAway);
+
+    var newRow = $("<tr>");
+    newRow.append("<td>" + sv.name + "</td>")
+    newRow.append("<td>" + sv.destination + "</td>")
+    newRow.append("<td>" + sv.frequency + "</td>")
+    newRow.append("<td>" + sv.nextTrain + "</td>")
+    newRow.append("<td>" + sv.minutesAway + "</td>")
+
+    $("tbody").append(newRow);
+  
+  });
+}
 
 $("#submitButton").on("click", function(event) {
   event.preventDefault();
 
   trainName = $("#train").val().trim();
-  destination = $("#destination").val().trim();
+  destinationName = $("#destination").val().trim();
   firstTime = $("#firstTrain").val().trim();
   frequency = $("#frequency").val().trim();
 
+  var timeConverted = moment(firstTime, "HH:mm").subtract(1, "years")
+  var diffTime = moment().diff(moment(timeConverted), "minutes");
+  var tRemainder = diffTime % frequency;
+  minutes = frequency - tRemainder;
+  nextTrain = moment().add(minutes, "minutes").format("hh:mm");
+  JSON.stringify(nextTrain);
+
   trainbase.ref().push({
     name: trainName,
-    destination: destination,
+    destination: destinationName,
     firstTrain: firstTime,
-    frequency: frequency
+    frequency: frequency,
+    nextTrain: nextTrain,
+    minutesAway: minutes
   });
 });
+
+dataPull();
