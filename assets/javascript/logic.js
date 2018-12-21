10b19c33e07bf2a15aecefbb042a2d37baa7c513
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2018-12-20 23:20:35 -0500

    Logic fixed

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,31 +17,38 @@ var trainName = "";
 var destination = "";
 var firstTime = "";
 var frequency = "";
-var nextTrain = "";
-var minutes = "";
-var currentTime = moment();
 
-function dataPull() {
-  trainbase.ref().on("child_added", function(snapshot){
-    var sv = snapshot.val();
-  
-    console.log(sv.name);
-    console.log(sv.destination);
-    console.log(sv.frequency);
-    console.log(sv.nextTrain);
-    console.log(sv.minutesAway);
+function timeCalc(firstTime, frequency) {
+  console.log(firstTime);
+  console.log(frequency);
+  var timeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  console.log(timeConverted);
+  var diffTime = moment().diff(moment(timeConverted), "minutes");
+  console.log(diffTime);
+  var tRemainder = diffTime % frequency;
+  console.log(tRemainder);
+  var minutes = frequency - tRemainder;
+  return minutes;
+};
 
-    var newRow = $("<tr>");
-    newRow.append("<td>" + sv.name + "</td>")
-    newRow.append("<td>" + sv.destination + "</td>")
-    newRow.append("<td>" + sv.frequency + "</td>")
-    newRow.append("<td>" + sv.nextTrain + "</td>")
-    newRow.append("<td>" + sv.minutesAway + "</td>")
+trainbase.ref().on("child_added", function(snapshot){
+  var sv = snapshot.val();
 
-    $("tbody").append(newRow);
-  
-  });
-}
+  var minutesAway = timeCalc(sv.firstTrain, sv.frequency);
+  var nextTrain = moment().add(minutesAway, "minutes").format("hh:mm");
+  JSON.stringify(nextTrain);
+
+  var newRow = $("<tr>");
+  newRow.append("<td>" + sv.name + "</td>")
+  newRow.append("<td>" + sv.destination + "</td>")
+  newRow.append("<td>" + sv.frequency + "</td>")
+  newRow.append("<td>" + nextTrain + "</td>")
+  newRow.append("<td>" + minutesAway + "</td>")
+
+  $("tbody").append(newRow);
+
+});
+
 
 $("#submitButton").on("click", function(event) {
   event.preventDefault();
@@ -51,21 +58,14 @@ $("#submitButton").on("click", function(event) {
   firstTime = $("#firstTrain").val().trim();
   frequency = $("#frequency").val().trim();
 
-  var timeConverted = moment(firstTime, "HH:mm").subtract(1, "years")
-  var diffTime = moment().diff(moment(timeConverted), "minutes");
-  var tRemainder = diffTime % frequency;
-  minutes = frequency - tRemainder;
-  nextTrain = moment().add(minutes, "minutes").format("hh:mm");
-  JSON.stringify(nextTrain);
 
   trainbase.ref().push({
     name: trainName,
     destination: destinationName,
     firstTrain: firstTime,
     frequency: frequency,
-    nextTrain: nextTrain,
-    minutesAway: minutes
   });
-});
 
-dataPull();
+  $(".form-control").val("");
+
+});
